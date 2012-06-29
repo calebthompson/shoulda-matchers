0253f2d26783cd71d505092fe86a4409b7628a64
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-06-29 15:32:10 -0500

    wip

--- spec/shoulda/active_model/positive_error_description_spec.rb
+++ spec/shoulda/active_model/positive_error_description_spec.rb
@@ -3,10 +3,15 @@ require 'spec_helper'
 
 describe Shoulda::Matchers::ActiveModel::PositiveErrorDescription do
   context 'with no errors' do
-    it 'shows it it expected errors'
+    it 'returns a description indicating that it expected at least one error' do
+      described_class.new([], 'expected message').description.should == 'errors to include "expected message", but none matched'
+    end
   end
 
   context 'with one error' do
-    it 'returns a description indicating that it got the expected errors'
+    it 'returns a description indicating that it got the expected errors' do
+      model = build_response
+      described_class.new(.matches?(
+    end
   end
 end
